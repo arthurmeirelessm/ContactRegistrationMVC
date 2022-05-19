ef8230a2d18cea7dcf38b8ec6f5e3439febdeff7
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-05-18 21:22:08 -0300

    Starting layout pages with Html

---
+++ ContactRegistrationMVC/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactRegistrationMVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+
+            
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
