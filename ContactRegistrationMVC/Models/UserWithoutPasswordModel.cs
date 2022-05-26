950726a1cb3979c0242c633a748332e56f8e2469
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-05-25 23:27:19 -0300

    Add validation to UserType in index

--- ContactRegistrationMVC/Models/UserWithoutPasswordModel.cs
+++ ContactRegistrationMVC/Models/UserWithoutPasswordModel.cs
@@ -10,14 +10,15 @@ namespace ContactRegistrationMVC.Models
 
         [Required(ErrorMessage = "Enter name")]
         public string Name { get; set; }
+      
+        [Required(ErrorMessage = "Enter login")]
+        public string Login { get; set; }
+
 
         [Required(ErrorMessage = "Enter your Email")]
         [EmailAddress(ErrorMessage = "The email is not valid")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Enter login")]
-        public string Login { get; set; }
 
-       
         [Required(ErrorMessage = "Enter your Type user")]
         public UserEnum UserType { get; set; }
       
