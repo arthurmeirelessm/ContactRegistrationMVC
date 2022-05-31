c9205ca65a61bbcccbe5fef526efa00d927259ca
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-05-31 20:05:11 -0300

    Create login route

--- ContactRegistrationMVC/Startup.cs
+++ ContactRegistrationMVC/Startup.cs
@@ -53,7 +53,7 @@ namespace ContactRegistrationMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
