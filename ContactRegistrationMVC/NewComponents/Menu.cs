0780adc060a46a0f262cd7c9bfa0fe38941cdfe4
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-01 22:19:02 -0300

    Component menu with invokeasync

---
+++ ContactRegistrationMVC/NewComponents/Menu.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ContactRegistrationMVC.NewComponents
+{
+    public class Menu : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+        }
+    }
+}
