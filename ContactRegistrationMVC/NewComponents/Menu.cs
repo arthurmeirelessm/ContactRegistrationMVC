b250b4850565c902b3a9c95e083a532b59b91cf9
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-01 23:15:37 -0300

    Finishing components in InvokeAsync

--- ContactRegistrationMVC/NewComponents/Menu.cs
+++ ContactRegistrationMVC/NewComponents/Menu.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContactRegistrationMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
 
 namespace ContactRegistrationMVC.NewComponents
@@ -7,7 +10,15 @@ namespace ContactRegistrationMVC.NewComponents
     {
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            string sessionUser = HttpContext.Session.GetString("sessionUserLogged");
+            if (string.IsNullOrEmpty(sessionUser))
+            {
+                return null;
+            }
 
+            UserModel user = JsonConvert.DeserializeObject<UserModel>(sessionUser);
+
+            return View(user);
         }
     }
 }
